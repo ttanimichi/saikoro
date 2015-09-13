d7514c2243526c4ba9d076b58f350444751989e8
Tsukuru Tanimichi <ttanimichi@hotmail.com>
2015-09-13 14:43:26 +0900

    Implement all methods naively

--- lib/saikoro.rb
+++ lib/saikoro.rb
@@ -3,13 +3,39 @@ require "saikoro/version"
 module Saikoro
   class << self
     def alphanumerics(length: 8)
-      Array.new(length).map { candidates.sample }.join
+      candidates = [*'a'..'z', *'A'..'Z', *'0'..'9']
+      generate(length, candidates)
+    end
+
+    def alphabets(length: 8)
+      candidates = [*'a'..'z', *'A'..'Z']
+      generate(length, candidates)
+    end
+
+    def lowercase_alphabets(length: 8)
+      candidates = [*'a'..'z']
+      generate(length, candidates)
+    end
+
+    def uppercase_alphabets(length: 8)
+      candidates = [*'A'..'Z']
+      generate(length, candidates)
+    end
+
+    def numbers(length: 8)
+      candidates = [*'0'..'9']
+      generate(length, candidates)
+    end
+
+    def ascii_printables(length: 8)
+      candidates = [*'!'..'~']
+      generate(length, candidates)
     end
 
     private
 
-    def candidates
-      @candidates ||= ['0'..'9', 'a'..'z', 'A'..'Z'].map(&:to_a).flatten
+    def generate(length, candidates)
+      length.times.map { candidates.sample }.join
     end
   end
 end
--- test/saikoro_test.rb
+++ test/saikoro_test.rb
@@ -2,14 +2,32 @@ require 'test_helper'
 
 class SaikoroTest < Minitest::Test
   def test_alphanumerics
-    target = Saikoro.alphanumerics
-    assert { target =~ /[0-9a-zA-Z]{8}/ }
-    assert { target.size == 8 }
+    assert { Saikoro.alphanumerics =~ /\A[0-9a-zA-Z]{8}\z/ }
+    assert { Saikoro.alphanumerics(length: 42) =~ /\A[0-9a-zA-Z]{42}\z/ }
   end
 
-  def test_alphanumerics_with_length
-    target = Saikoro.alphanumerics(length: 16)
-    assert { target =~ /[0-9a-zA-Z]{16}/ }
-    assert { target.size == 16 }
+  def test_alphabets
+    assert { Saikoro.alphabets =~ /\A[a-zA-Z]{8}\z/}
+    assert { Saikoro.alphabets(length: 42) =~ /\A[a-zA-Z]{42}\z/ }
+  end
+
+  def test_lowercase_alphabets
+    assert { Saikoro.lowercase_alphabets =~ /\A[a-z]{8}\z/}
+    assert { Saikoro.lowercase_alphabets(length: 42) =~ /\A[a-z]{42}\z/ }
+  end
+
+  def test_uppercase_alphabets
+    assert { Saikoro.uppercase_alphabets =~ /\A[A-Z]{8}\z/}
+    assert { Saikoro.uppercase_alphabets(length: 42) =~ /\A[A-Z]{42}\z/ }
+  end
+
+  def test_numbers
+    assert { Saikoro.numbers =~ /\A[0-9]{8}\z/ }
+    assert { Saikoro.numbers(length: 42) =~ /\A[0-9]{42}\z/ }
+  end
+
+  def test_ascii_printables
+    assert { Saikoro.ascii_printables =~ /\A[\x21-\x7e]{8}\z/ }
+    assert { Saikoro.ascii_printables(length: 42) =~ /\A[\x21-\x7e]{42}\z/ }
   end
 end
